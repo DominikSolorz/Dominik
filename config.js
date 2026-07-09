8e0eca55def9e8b998e1df5eb82852c58abd0836
DominikSolorz <251066599+DominikSolorz@users.noreply.github.com>
2026-07-09 18:11:11 +0200

    Update apkUrl in LINKTALK_CONFIG

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.LINKTALK_CONFIG = {
   supabaseUrl: "https://fpscmykuarvmdgnknmnr.supabase.co",
   supabaseAnonKey: "sb_publishable_-Btbv6z539KeYJ-DZZ1-Qw_pS70O4A0",
   publicAppUrl: "https://linktalk.pl/",
-  apkUrl: ""
+  apkUrl: "./downloads/linktalk-debug.apk"
 };
 
 window.BLISKOCHAT_CONFIG = window.LINKTALK_CONFIG;
