e260c5831c43fde1ad00b6f1f489ef59a6caf8e9
DominikSolorz <dominiksolorz@help10.hub.pl>
2026-09-04 07:00:31 +0200

    Usuń wszystkie pliki z repozytorium

--- ovh/release/site/config.example.js
+++
@@ -1,8 +0,0 @@
-window.LINKTALK_CONFIG = {
-  supabaseUrl: "https://YOUR_PROJECT_REF.supabase.co",
-  supabaseAnonKey: "YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY",
-  publicAppUrl: "https://linktalk.pl/",
-  apkUrl: "./downloads/linktalk-debug.apk"
-};
-
-window.BLISKOCHAT_CONFIG = window.LINKTALK_CONFIG;
